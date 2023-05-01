ce7540bc8ddfe73a9261140600987d5eef184cbb
Obiora <obiorakingsley5050@gmail.com>
2023-05-01 14:34:59 +0100

    palindrome solution

---
+++ version001.js
@@ -0,0 +1,19 @@
+// write a JavaScript function that checks whether a passed string is a palindrome or not?
+
+function checkPalindrome(string) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < Math.floor(len / 2); i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+console.log(checkPalindrome("madam"));
